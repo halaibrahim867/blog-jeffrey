b67fae54e87cd9132b4b93631177cc53e3cfad88
hala ibrahim <halaibrahim867@gmail.com>
2023-11-04 17:39:49 +0200

    add blade files and make posts and post page dynamic

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +17,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts'=>Post::latest()->get()
+    ]);
+
+});
+
+Route::get('posts/{post:slug}',function (Post $post){
+    return view('post',['post'=>$post]);
+});
+
+Route::get('categories/{category:slug}',function (Category $category){
+
+    return view('posts',[
+        'posts'=>$category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}',function (User $author){
+
+    return view('posts',[
+        'posts'=>$author->posts
+    ]);
 });
