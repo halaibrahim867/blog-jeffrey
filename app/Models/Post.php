83a3083db7d6b651715e560f913198c0b8c59f18
hala ibrahim <halaibrahim867@gmail.com>
2023-11-07 17:11:17 +0200

    add pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,9 +15,10 @@ class Post extends Model
     public function scopeFilter($query,array $filters)
     {
         $query->when($filters['search'] ?? false, fn($query, $search)=>
-            $query
-                 ->where('title','like','%' . $search . '%')
+            $query->where(fn($query)=>
+                 $query->where('title','like','%' . $search . '%')
                 ->orWhere('body','like','%'. $search.'%')
+            )
         );
 
         $query->when($filters['category'] ?? false, fn($query, $category)=>
@@ -29,7 +30,7 @@ class Post extends Model
         $query->when($filters['author'] ?? false, fn($query, $author)=>
             $query->whereHas('author',fn($query)=>
                 $query->where('username',$author)
-        )
+            )
         );
 
     }
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -10,6 +10,7 @@
         <div class="relative  lg:inline-flex  bg-gray-100 rounded-xl">
             <x-category-dropdown />
         </div>
+{{--
 
         <!-- Other Filters -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
@@ -32,10 +33,16 @@
                 </g>
             </svg>
         </div>
+--}}
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="/">
+
+                @if(request('category'))
+                    <input type="hidden" name="category" value="{{request('category')}}">
+                @endif
+
                 <input type="text" name="search" placeholder="Find something"
                        class="bg-transparent placeholder-black font-semibold text-sm"
                         value="{{request('search')}}">
