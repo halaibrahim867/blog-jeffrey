6cf0c2d1ff981513f38c6ad81159ef75ff43c2fe
hala ibrahim <halaibrahim867@gmail.com>
2023-11-10 18:42:46 +0200

    add comment section

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,6 +11,14 @@ class PostsTest extends TestCase
 {
     public function test_home_page_contain_posts(): void
     {
+        Post::create([
+            'user_id'=>1,
+            'category_id'=>6,
+            'title'=>'Test Post',
+            'slug'=>'test-post',
+            'excerpt'=>'excerpt for test post',
+            'body'=>'body for test post'
+        ]);
         $response = $this->get('/');
 
         $response->assertStatus(200);
